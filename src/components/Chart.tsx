a803e67709c101d3a756501ab60434cfb2fffa53
Daniel… <danieloluwafolajimi@gmail.com>
2024-10-22 12:26:03 +0100

    Add a chart image and display it

---
+++ src/components/Chart.tsx
@@ -0,0 +1,12 @@
+import ChartImage from "../assets/images/chart.svg";
+
+const Chart = () => {
+    return (
+        <div>
+            <h2>Event Registration per month</h2>
+            <img src={ChartImage} alt="image of a chart" />
+        </div>
+    );
+};
+
+export default Chart;
