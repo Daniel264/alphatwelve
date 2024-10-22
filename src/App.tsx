d71459f16cd691c5f2585597a93a51a2c35aa646
Daniel… <danieloluwafolajimi@gmail.com>
2024-10-22 15:01:41 +0100

    Add a menu component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import Hero from "./components/Hero";
+import Menu from "./components/Menu";
 import Summary from "./components/Summary";
 import "./globals.css";
 
 function App() {
     return (
-        <div className="w-full h-[100vh] p-4 md:p-8 flex-col justify-center items-center flex">
-            <Summary />
-            <Hero />
+        <div className="flex h-[100vh] ">
+          <div><Menu /></div>
+          <div className="w-full h-[100vh] p-4 md:p-8 flex-col justify-center items-center flex">
+              <Summary />
+              <Hero />
+          </div>
         </div>
     );
 }
