9c8b7e465bf04449f702bb44063f40d59d51065f
Daniel… <danieloluwafolajimi@gmail.com>
2024-10-22 15:08:04 +0100

    Style the menu component

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,9 +1,9 @@
-import React from "react";
+
 
 const Menu = () => {
     return (
         <div>
-            <ul className="menu bg-base-200 rounded-box w-56">
+            <ul className="menu border-[#ADA9BB]/20 border-r-2 h-[100vh] rounded-none w-56">
                 <li>
                     <a>
                         <svg
@@ -20,7 +20,7 @@ const Menu = () => {
                                 d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
                             />
                         </svg>
-                        Item 2
+                        Home
                     </a>
                 </li>
                 <li>
@@ -39,7 +39,7 @@ const Menu = () => {
                                 d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
                             />
                         </svg>
-                        Item 1
+                        Info
                     </a>
                 </li>
                 <li>
@@ -58,7 +58,7 @@ const Menu = () => {
                                 d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"
                             />
                         </svg>
-                        Item 3
+                        Insights
                     </a>
                 </li>
             </ul>
