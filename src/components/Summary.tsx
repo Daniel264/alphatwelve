93de529bf65320c3211a820984dbce78a48b44fd
Daniel… <danieloluwafolajimi@gmail.com>
2024-10-22 21:19:26 +0100

    Refactor Summary component layout and add welcome message for improved user experience

--- src/components/Summary.tsx
+++ src/components/Summary.tsx
@@ -2,27 +2,30 @@ import SummaryComponent from "./SummaryComponent";
 
 const Summary = () => {
     return (
-        <section className="flex flex-row gap-10 w-full">
-            <SummaryComponent
-                title={"Total Events"}
-                price={100000}
-                percentagePositive={true}
-            />
-            <SummaryComponent
-                title={"Active Speakers"}
-                price={25}
-                percentagePositive={false}
-            />
-            <SummaryComponent
-                title={"Total Registrations"}
-                price={300}
-                percentagePositive={true}
-            />
-            <SummaryComponent
-                title={"Total Revenue"}
-                price={500000}
-                percentagePositive={true}
-            />
+        <section className="flex flex-col w-full text-left">
+            <h1 className="font-medium text-2xl pb-5">Welcome! here’s your summary</h1>
+            <div className="flex flex-row gap-10 w-full">
+                <SummaryComponent
+                    title={"Total Events"}
+                    price={100000}
+                    percentagePositive={true}
+                />
+                <SummaryComponent
+                    title={"Active Speakers"}
+                    price={25}
+                    percentagePositive={false}
+                />
+                <SummaryComponent
+                    title={"Total Registrations"}
+                    price={300}
+                    percentagePositive={true}
+                />
+                <SummaryComponent
+                    title={"Total Revenue"}
+                    price={500000}
+                    percentagePositive={true}
+                />
+            </div>
         </section>
     );
 };
