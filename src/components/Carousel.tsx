939c0cd9652ec30b3c518de06baabf01fda18f2c
Daniel… <danieloluwafolajimi@gmail.com>
2024-10-22 12:03:40 +0100

    Give all the carousel images their correct unique identifiers

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -8,48 +8,56 @@ const Carousel = () => {
         <div className="w-full md:w-[50%] ">
             <div className="carousel w-full h-[400px]">
                 <div id="slide1" className="carousel-item relative w-full">
-                    <img
-                        src={slideOne}
-                        className="w-full"
-                    />
-                    <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-                        <a href="#slide4" className="btn btn-circle">
+                    <img src={slideOne} className="w-full" />
+                    <div className="absolute left-20 right-20 top-1/2 flex -translate-y-1/2 transform justify-between">
+                        <a
+                            href="#slide1"
+                            className="btn btn-circle bg-white border-none"
+                        >
                             ❮
                         </a>
-                        <a href="#slide2" className="btn btn-circle">
+                        <a
+                            href="#slide2"
+                            className="btn btn-circle bg-white border-none"
+                        >
                             ❯
                         </a>
                     </div>
                 </div>
                 <div id="slide2" className="carousel-item relative w-full">
-                    <img
-                        src={slideTwo}
-                        className="w-full"
-                    />
-                    <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-                        <a href="#slide1" className="btn btn-circle">
+                    <img src={slideTwo} className="w-full" />
+                    <div className="absolute left-20 right-20 top-1/2 flex -translate-y-1/2 transform justify-between">
+                        <a
+                            href="#slide2"
+                            className="btn btn-circle bg-white border-none"
+                        >
                             ❮
                         </a>
-                        <a href="#slide3" className="btn btn-circle">
+                        <a
+                            href="#slide3"
+                            className="btn btn-circle bg-white border-none"
+                        >
                             ❯
                         </a>
                     </div>
                 </div>
                 <div id="slide3" className="carousel-item relative w-full">
-                    <img
-                        src={slideThree}
-                        className="w-full"
-                    />
-                    <div className="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform justify-between">
-                        <a href="#slide2" className="btn btn-circle">
+                    <img src={slideThree} className="w-full" />
+                    <div className="absolute left-20 right-20 top-1/2 flex -translate-y-1/2 transform justify-between">
+                        <a
+                            href="#slide3"
+                            className="btn btn-circle bg-white border-none"
+                        >
                             ❮
                         </a>
-                        <a href="#slide4" className="btn btn-circle">
+                        <a
+                            href="#slide1"
+                            className="btn btn-circle bg-white border-none"
+                        >
                             ❯
                         </a>
                     </div>
                 </div>
-               
             </div>
         </div>
     );
