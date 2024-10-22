05cbb3eaa5c2f046d5bfdf1e5d586fc1cec7b1dd
Daniel… <danieloluwafolajimi@gmail.com>
2024-10-22 21:15:17 +0100

    Enhance Hero and SummaryComponent styles for improved readability and layout

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,7 +6,9 @@ const Hero = () => {
     return (
         <div className="h-fit flex flex-col w-full justify-center">
             <div className="w-[50%] flex justify-center pb-4">
-                <h2>Event registrations per month</h2>
+                <h2 className="text-xl font-semibold">
+                    Event registrations per month
+                </h2>
             </div>
             <div className="h-fit flex flex-row w-full justify-center">
                 <Chart />
--- src/components/SummaryComponent.tsx
+++ src/components/SummaryComponent.tsx
@@ -22,13 +22,18 @@ const SummaryComponent = ({
                 </div>
                 <div className="flex flex-row space-x-5">
                     <p className="font-medium text-xl">{price}</p>
-                    <p>
+                    <span className="flex">
                         {percentagePositive ? (
                             <img src={Incline} />
                         ) : (
                             <img src={Decline} />
                         )}
-                    </p>
+                        <span
+                            className={`${percentagePositive ? `text-green-500` : `text-red-500`} font-medium flex`}
+                        >
+                            <span>{percentagePositive ? "+" : "-"}</span> 5.0%
+                        </span>
+                    </span>
                 </div>
             </article>
         </div>
