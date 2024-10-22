462d1bf87fe3fa8f50ad396343084b181ae1f0d4
Daniel… <danieloluwafolajimi@gmail.com>
2024-10-22 12:38:32 +0100

    Add the chart text and align it

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,9 +4,14 @@ import Carousel from "./Carousel";
 
 const Hero = () => {
     return (
-        <div className="h-fit flex flex-row">
-            <Chart />
-            <Carousel />
+        <div className="h-fit flex flex-col w-full justify-center">
+            <div className="w-[50%] flex justify-center pb-4">
+                <h2>Event registrations per month</h2>
+            </div>
+            <div className="h-fit flex flex-row w-full justify-center">
+                <Chart />
+                <Carousel />
+            </div>
         </div>
     );
 };
